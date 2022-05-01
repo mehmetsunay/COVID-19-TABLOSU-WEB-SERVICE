c1dc1c20f74e9015f46e9c60ceb752c8f06adfa8
Mehmet Sunay <mehmetsunay@windowslive.com>
2022-05-01 10:07:38 +0300

    Covid 19 Tablosu-Web Service
    Covid 19 Tablosu-Web Service

---
+++ WindowsFormsCovid19/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace WindowsFormsCovid19
+{
+    public partial class frm_covid : MetroFramework.Forms.MetroForm
+    {
+        public frm_covid()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXml("https://api.memleketyazilim.com/covid-19-turkey-master/dataset/timeline.xml");
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+    }
+}
